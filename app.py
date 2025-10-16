4a0698756d160c017fff4e8dd3a21af9cfeeccea
juliadaser <bessj006@newschool.edu>
2025-10-15 21:21:12 -0400

    digitaloceanexample

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-import os
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
-# --- Flask setup ---
 app = Flask(__name__)
 
-# --- Main route ---
-@app.route("/", methods=["GET", "POST"])
-def random_function():
-    
-    variable_here = True
 
-    return render_template(
-        "index.html",
-        variable_here= variable_here,
-    )
-
-# --- Run app ---
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
